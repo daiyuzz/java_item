26fcec112a2324b6c1ab155917ec5e034081b118
daiyu <nicai1993>
2020-12-20 17:01:14 +0800

    添加 PmsBrandService 实现类

---
+++ mall-tiny/src/main/java/com/macro/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,53 @@
+package com.macro.mall.tiny.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.macro.mall.tiny.mbg.mapper.PmsBrandMapper;
+import com.macro.mall.tiny.mbg.model.PmsBrand;
+import com.macro.mall.tiny.mbg.model.PmsBrandExample;
+import com.macro.mall.tiny.service.PmsBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * daiyu
+ * 2020/12/20
+ * PmsBrandService 实现类
+ */
+public class PmsBrandServiceImpl implements PmsBrandService {
+
+    @Autowired
+    private PmsBrandMapper pmsBrandMapper;
+
+    @Override
+    public List<PmsBrand> listAllBrand() {
+        return pmsBrandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public int createBrand(PmsBrand pmsBrand) {
+        return pmsBrandMapper.insertSelective(pmsBrand);
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrand pmsBrandDto) {
+        pmsBrandDto.setId(id);
+        return pmsBrandMapper.updateByPrimaryKeySelective(pmsBrandDto);
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        return pmsBrandMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public List<PmsBrand> listBrand(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        return pmsBrandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public PmsBrand getBrand(Long id) {
+        return pmsBrandMapper.selectByPrimaryKey(id);
+    }
+}
