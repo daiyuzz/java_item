f839d725f416e0227c394e9398f8c6b8f0cb2e39
daiyu <daiyu_zz@163.com>
2021-10-18 22:06:22 +0800

    getway

---
+++ cloud-demo/gateway/src/main/java/cn/itcase/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.itcase.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dyx on 2021/10/16
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
