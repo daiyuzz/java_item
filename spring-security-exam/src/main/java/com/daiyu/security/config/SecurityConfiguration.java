34d80a715cd2e8c27cb05be349695e0c8d9f8f56
daiyu_zz <daiyu_zz@163.com>
2021-11-11 21:11:02 +0800

    11.11

--- spring-security-exam/src/main/java/com/daiyu/security/config/SecurityConfiguration.java
+++ spring-security-exam/src/main/java/com/daiyu/security/config/SecurityConfiguration.java
@@ -2,11 +2,13 @@ package com.daiyu.security.config;
 
 import com.daiyu.security.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
  * created by dyx on 2021/10/27
